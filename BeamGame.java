be899e6450d10c904b0bdeea68f5fa7d18d0b1ab
gotop44 <gotop4455@gmail.com>
2016-03-20 23:55:02 +0800

    Add

---
+++ BeamGame.java
@@ -0,0 +1,47 @@
+/** U10416041 */
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class BeamGame extends Application {
+
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Beam Game");
+
+        initRootLayout();
+    }
+	
+    public void initRootLayout() {
+        try {
+            
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(BeamGame.class.getResource("BeamMachine.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+		catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
